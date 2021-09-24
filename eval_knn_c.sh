a8df35f02214b6ec8bf72f91b89d7611ad931fd2
Chaowei Xiao <xiaocw@umich.edu>
2021-09-24 14:20:24 -0400

    update

--- eval_knn_c.sh
+++ eval_knn_c.sh
@@ -7,4 +7,4 @@ ARCH=$2
 
 # python -m torch.distributed.launch --nproc_per_node=4 eval_linear.py --data_path $DATA_PATH --output_dir $OUT_PATH/lincls/epoch0300 --pretrained_weights $CKPT_PATH --checkpoint_key teacher --batch_size_per_gpu 256 --arch swin_tiny --cfg experiments/imagenet/swin/swin_tiny_patch4_window7_224.yaml --n_last_blocks 4 --num_labels 1000 MODEL.NUM_CLASSES 0
 
-python -m torch.distributed.launch --nproc_per_node=1 eval_knn_c.py --data_path $DATA_PATH --dump_features $OUT_PATH/$ARCH/features --pretrained_weights $CKPT_PATH --checkpoint_key teacher --batch_size_per_gpu 256 --arch $ARCH --cfg experiments/imagenet/swin/swin_tiny_patch4_window7_224.yaml MODEL.NUM_CLASSES 0
+CUDA_VISIBLE_DEVICES=3 python -m torch.distributed.launch --nproc_per_node=1 eval_knn_c.py --data_path $DATA_PATH --dump_features $OUT_PATH/$ARCH/features --pretrained_weights $CKPT_PATH --checkpoint_key teacher --batch_size_per_gpu 256 --arch $ARCH --cfg experiments/imagenet/swin/swin_tiny_patch4_window7_224.yaml MODEL.NUM_CLASSES 0
